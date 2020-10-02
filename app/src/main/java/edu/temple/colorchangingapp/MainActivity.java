c85ff1901ae540cee708d22732864545665ca338
ShigureChen <70284977+ShigureChen@users.noreply.github.com>
2020-10-02 23:51:02 +0800

    Complete main activity with girdView, second activity is still under construction.

--- app/src/main/java/edu/temple/colorchangingapp/MainActivity.java
+++ app/src/main/java/edu/temple/colorchangingapp/MainActivity.java
@@ -2,14 +2,62 @@ package edu.temple.colorchangingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] colourStr = new String[9];
+
+        colourStr[0] = "Red";
+        colourStr[1] = "Yellow";
+        colourStr[2] = "Cyan";
+        colourStr[3] = "Light Gray";
+        colourStr[4] = "Magenta";
+        colourStr[5] = "Silver";
+        colourStr[6] = "Blue";
+        colourStr[7] = "Green";
+        colourStr[8] = "Dark Gray";
+
+        final int[] colourInt = new int[9];
+
+        colourInt[0] = Color.RED;
+        colourInt[1] = Color.YELLOW;
+        colourInt[2] = Color.CYAN;
+        colourInt[3] = Color.GRAY;
+        colourInt[4] = Color.MAGENTA;
+        colourInt[5] = Color.LTGRAY;
+        colourInt[6] = Color.BLUE;
+        colourInt[7] = Color.GREEN;
+        colourInt[8] = Color.DKGRAY;
+
+        setTitle("Color Changing App");
+
+        gridView = findViewById(R.id.gridView);
+        final GridAdapter adapter = new GridAdapter(this, colourStr, colourInt);
+        gridView.setAdapter(adapter);
+        gridView.bringToFront();
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(context, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
